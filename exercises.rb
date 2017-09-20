16901bfd53ec13dd2ae255c1213f8b0e0fe661e2
ANTONIO <ANCLIST@GMAIL.COM>
2017-09-20 18:05:50 -0400

    Fix syntax on Arrays and create first hash

--- exercises.rb
+++ exercises.rb
@@ -1,5 +1,11 @@
 fav_colours = ["blue", "dark blue", "navy blue", "matte blue"]
 family_age = [34, 35, 33, 32, 37, 28]
-heads_or-tails = ["heads", "tails", "heads", "tails", "heads"]
+heads_or_tails = ["heads", "tails", "heads", "tails", "heads"]
 fav_artists = ["Marc Anthony", "Carlos Vives", "Ruben Blades"]
-fav_colours_as_symbols [:blue, :dark_blue, :navy_blue, :matte_blue]
+fav_colours_as_symbols = [:blue, :dark_blue, :navy_blue, :matte_blue]
+
+definitions = {}
+
+definitions [:up] = "toward the sky or a higher position."
+definitions [:down] = "toward or in a lower place or position, especially to or on the ground or another surface"
+definitions [:side] = "a position to the left or right of an object, place, or central point."
