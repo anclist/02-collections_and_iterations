bff0c113fff28a317c86f08d5500a8a790137127
ANTONIO <ANCLIST@GMAIL.COM>
2017-09-24 10:27:08 -0400

    Make excercise 10 pretty

--- exercise10.rb
+++ exercise10.rb
@@ -1,12 +1,12 @@
 numbers = (1..100)
 
 numbers.each do |number|
-  number%3 && number%5
-  if number%3 == 0 && number%5 == 0
+  number % 3 && number % 5
+  if number % 3 == 0 && number % 5 == 0
     puts "BitMaker"
-  elsif number%3 == 0 && number%5 != 0
+  elsif number % 3 == 0 && number % 5 != 0
     puts "Bit"
-  elsif number%3 != 0 && number%5 == 0
+  elsif number % 3 != 0 && number % 5 == 0
     puts "Maker"
   else
     puts number
